24665334aec7b270024ff0254b190e01b6f40b48
Yury Namgung <yury.namgung@gmail.com>
2019-04-02 00:04:39 -0700

    update hangingNode.py
    checks if there are hanging nodes aka code that is outsourced to but does not outsource

--- hangingNode.py
+++ hangingNode.py
@@ -23,3 +23,25 @@ print(list[0])
 col1, col2 = splitColumns(list)
 print(col1[1])
 print(col2[1])
+
+
+#array 1:outsourcing link
+#array 2:links to other things
+from array import *
+
+my_array1 = array('i', [1,2,3,4,5,6,7,8,9,10])
+my_array2 = array('i', [1,2,5,11,22,33,44,77])
+my_array3 = array('i', [1,2,3,4,5,6,7,8,9,10])
+my_array4 = array('i', [40,50,60,11,22,33,44,77])
+my_array5 = array('i', [1,2,3,4,5,6,7,8,9,10])
+
+def elementsInCommon(array1, array2):
+    i = 0
+    for i in range(len(array2)):
+        if array2[i] not in array1:
+            print(i, array2[i])
+    print("These are dangling nodes.")
+
+
+#subwebs not connected to any other things
+#assuming that there are no dangling nodes
